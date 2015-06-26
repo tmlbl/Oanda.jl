5fc2b5137c3c0c820bb484b571a8d6e9f7618bae
Tim Lebel <tim.lebel@gmail.com>
2015-06-26 00:30:03 -0700

    updates

--- src/Oanda.jl
+++ src/Oanda.jl
@@ -1,42 +1,35 @@
+module Oanda
+
+using Requests,
+      Dates,
+      JSON
+
+export prices,
+       past
+
 include("Qstring.jl")
 
-using Requests
+const baseuri = "http://api-sandbox.oanda.com/"
+const defheaders = Dict{String,String}("X-Accept-Datetime-Format" => "UNIX")
 
-baseuri = "http://api-sandbox.oanda.com/"
-defheaders = {"X-Accept-Datetime-Format" => "UNIX"}
+immutable Candle
+  time::DateTime
+  bid::Float64
+  ask::Float64
+end
 
-function getCandles(from::DateTime, to::DateTime, instrument::String)
+function prices(from::DateTime, to::DateTime, instrument::String)
+  println("Loading candles for "*instrument*" from "*string(from)*" to "*string(to))
   query = qstring(("start", from), ("end", to), ("instrument", instrument))
   uri = string(baseuri, "v1/candles", query)
-  JSON.parse(get(uri, headers = defheaders).data)
+  candles = Array{Candle,1}()
+  for c in JSON.parse(get(uri, headers = defheaders).data)["candles"]
+    push!(candles, Candle(Dates.unix2datetime(parse(c["time"])), c["closeAsk"], c["closeBid"]))
+  end
+  println("Loaded "*string(length(candles))*" candles")
+  candles
 end
 
-println("Loading candle data...")
-cdata = getCandles(Dates.now() - Dates.Hour(2), Dates.now(), "USD_CAD")
+past(hours::Int) = prices(Dates.now() - Dates.Hour(hours), Dates.now(), "USD_CAD")
 
-println("Loading events CSV...")
-events = readcsv("julia/fix/events.csv")
-
-events[4]
-# A type to store the event data
-type Event
-  title::String
-  impact::Int
-  time::DateTime
-  country::String
-end
-
-# An array of events
-forexevents = Array(Event, int(length(events) / 4))
-
-map((rawevent) -> 10, forexevents)
-
-Event("Bool", Dates.now(), 1, "f")
-
-for i = events
-  forexevents[i] = Event(events[i]["title"])
-end
-
-function getEventDelta(e::Event)
-  e[1]
-end
+end # module Oanda
--- src/Qstring.jl
+++ src/Qstring.jl
@@ -5,7 +5,7 @@ using Dates
 # given Julia DateTimes
 function qstring(kv::Tuple...)
   encodeval(v::String) = encodeURI(v)
-  encodeval(dt::DateTime) = string(int64(datetime2unix(dt)))
+  encodeval(dt::DateTime) = string(round(Int64, Dates.datetime2unix(dt)))
   encodepair(p::Tuple) = string(encodeval(p[1]), "=", encodeval(p[2]), "&")
   q = string("?", join(map(encodepair, kv)))
   q[1:length(q) - 1]
@@ -13,4 +13,4 @@ end
 
 # Let's test it!
 @assert qstring(("a", "b"), ("c", "d")) == "?a=b&c=d"
-@assert qstring(("date", unix2datetime(1424888079))) == "?date=1424888079"
+@assert qstring(("date", Dates.unix2datetime(1424888079))) == "?date=1424888079"
